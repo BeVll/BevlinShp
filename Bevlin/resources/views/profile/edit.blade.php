229695676c77ab34f7572d2294bd0760cdf206d0
BeVl <vladixerplay@gmail.com>
2022-12-24 19:50:30 +0200

    profile_page

--- Bevlin/resources/views/profile/edit.blade.php
+++ Bevlin/resources/views/profile/edit.blade.php
@@ -4,24 +4,35 @@
 
 
     <div style="padding-top: 90px">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="shadow p-3 mb-5 bg-white rounded">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 shadow p-3 row mb-5 bg-white rounded">
+            <div class="col-3 row">
+                <div class="col-11">
+                    <ul class="list-group profile_menu">
+                        <li class="list-group-item active" aria-current="true">An active item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                    </ul>
+                </div>
+
+                <div class="vr col-auto" style="width: 1px"></div>
+            </div>
+
+            <div class="col-9">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
-            </div>
-
-            <div class="shadow p-3 mb-5 bg-white rounded">
+                <hr>
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
+
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
             </div>
 
-            <div class="shadow p-3 mb-5 bg-white rounded">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
         </div>
     </div>
 
